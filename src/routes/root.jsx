b53cdc2ec91a995be32d84941bf29b0ea3dd4268
Anthony Victorio <anthonyvictorio@Anthonys-MacBook-Pro.local>
2024-04-08 19:33:44 -0400

    Added New button functionality

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,9 +2,15 @@ import {
     Outlet,
     Link,
     useLoaderData,
+    Form,
 } 
 from "react-router-dom"; 
-import { getContacts } from "../contacts";
+import { getContacts, createContact } from "../contacts";
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 export async function loader(){
     const contacts = await getContacts();
@@ -36,9 +42,9 @@ export default function Root() {
                 aria-live="polite"
               ></div>
             </form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
+            <Form method="post">
+            <button type="submit">New</button>
+          </Form>
           </div>
           <nav>
           {contacts.length ? (
