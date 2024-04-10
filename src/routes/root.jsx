405e67ff428517303ea07a29020b8ea8279565b2
Anthony Victorio <anthonyvictorio@Anthonys-MacBook-Pro.local>
2024-04-10 19:58:00 -0400

    cancel button and search susbmission functional

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -9,19 +9,26 @@ import {
 } 
 from "react-router-dom"; 
 import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
+
+useEffect(() => {
+  document.getElementById("q").value = q;
+}, [q]);
 
 export async function action() {
   const contact = await createContact();
   return redirect(`/contacts/${contact.id}/edit`);
 }
 
-export async function loader(){
-    const contacts = await getContacts();
-    return {contacts};
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export default function Root() {
-    const {contacts} = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
 
     return (
@@ -29,13 +36,14 @@ export default function Root() {
         <div id="sidebar">
           <h1>React Router Contacts</h1>
           <div>
-            <form id="search-form" role="search">
+          <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
+                defaultValue={q}
               />
               <div
                 id="search-spinner"
@@ -46,7 +54,7 @@ export default function Root() {
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
             <button type="submit">New</button>
           </Form>
